adbc857e9e950d9d5485410835849be157f7a0e5
Andrei Tarassov <tamogenia@gmail.com>
2026-05-13 19:27:49 +0300

    Added Redame v1

--- src/main/java/com/app/game/tetris/configuration/WebSocketConfig.java
+++ src/main/java/com/app/game/tetris/configuration/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.app.game.tetris.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
